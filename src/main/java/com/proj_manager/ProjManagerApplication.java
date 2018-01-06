a80ff543a2ee42bef71f7ebc08da396aee6bf395
OstrowskiJ <jakub.ostrowski1404@gmail.com>
2018-01-06 12:03:35 +0100

    Initial commit

---
+++ src/main/java/com/proj_manager/ProjManagerApplication.java
@@ -0,0 +1,12 @@
+package com.proj_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjManagerApplication.class, args);
+	}
+}
